921105c2f02db3a8172fe0442a61e215e09d9dcb
HaoyangQi <haoyangq@gmail.com>
2020-08-06 15:51:48 -0400

    Cleanup projects

--- application-test/windesign.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "resource.h"
-
-BOOL InitWindowDesignerInstance(HINSTANCE hInstance, HWND hParent, int nCmdShow);
-void MoveTargetWindow(int delta_horizontal, int delta_vertical);
-void ScaleTargetWindow(int delta_width, int delta_height);
-void WindowDesignerRefresh();
-void WindowDesignerRefreshEx(HDC hdcMain);
